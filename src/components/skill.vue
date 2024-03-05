3593cb740b86c5fc3c49fe511f3c062d04ca2374
Naoya-Yasuda <2301330039zz@cyber-u.ac.jp>
2024-03-05 19:34:09 +0900

    アイコンエリアcss調整

--- src/components/skill.vue
+++ src/components/skill.vue
@@ -4,7 +4,7 @@
 
     <div class="quolifications">
       <h3>- Quolifications -</h3>
-      <div class="icon-area" style="display:flex">
+      <div class="icon-area">
         <figure><img src="../assets/images/skills/CSM-logo.png" alt="Certified_ScrumMaster"><figcaption>Certified ScrumMaster</figcaption></figure>
         <figure><img src="../assets/images/skills/SAA-logo.png" alt="AWS_Certified_Solutions_Architect"><figcaption>AWS-SAA</figcaption></figure>
         <figure><img src="../assets/images/skills/AP-logo.png" alt="Applied_Information_Technology_Engineer"><figcaption>応用情報技術者</figcaption></figure>
@@ -16,23 +16,27 @@
     </div>
 
     <div class="activity">
+      <div class="icon-area">
       <h3>- Activity -</h3>
-      <figure><img src="../assets/images/skills/Hack_U_Online.png" alt="Hack_U_Online"><figcaption>Hack U Online2023<br>最優秀賞<br>HappyHacking賞</figcaption></figure>
-      <figure><img src="../assets/images/skills/MDXQ-1.png" alt="MDXQ_Term1"><figcaption>経産省主催マナビDX Quest2023 Term1<br>Gold修了</figcaption></figure>
-      <figure><img src="../assets/images/skills/MDXQ-2.png" alt="MDXQ_Term2"><figcaption>経産省主催マナビDX Quest2023 Term2 地域協業プログラム<br>修了</figcaption></figure>
-      <figure><img src="../assets/images/skills/zero-to-one-logo.png" alt="zero_to_one"><figcaption>JDLA「E資格」向け認定プログラム<br>修了</figcaption></figure>
-      <figure><img src="../assets/images/skills/Kaggle-logo.png" alt="kaggle"><figcaption>Kaggle</figcaption></figure>
-      <figure><img src="../assets/images/skills/SIGNATE-logo.png" alt="zero_to_one"><figcaption>SIGNATE</figcaption></figure>
+        <figure><img src="../assets/images/skills/Hack_U_Online.png" alt="Hack_U_Online"><figcaption>Hack U Online2023<br>最優秀賞<br>HappyHacking賞</figcaption></figure>
+        <figure><img src="../assets/images/skills/MDXQ-1.png" alt="MDXQ_Term1"><figcaption>経産省主催マナビDX Quest2023<br>Term1 Gold</figcaption></figure>
+        <figure><img src="../assets/images/skills/MDXQ-2.png" alt="MDXQ_Term2"><figcaption>経産省主催マナビDX Quest2023<br>Term2 地域協業プログラム</figcaption></figure>
+        <figure><img src="../assets/images/skills/zero-to-one-logo.png" alt="zero_to_one"><figcaption>JDLA「E資格」向け<br>認定プログラム</figcaption></figure>
+        <figure><img src="../assets/images/skills/Kaggle-logo.png" alt="kaggle"><figcaption>Kaggle</figcaption></figure>
+        <figure><img src="../assets/images/skills/SIGNATE-logo.png" alt="zero_to_one"><figcaption>SIGNATE</figcaption></figure>
+      </div>
     </div>
 
     <div class="languages">
+      <div class="icon-area">
       <h3>- Languages -</h3>
-      <figure><img src="../assets/images/skills/html.png" alt="html"><figcaption>HTML</figcaption></figure>
-      <figure><img src="../assets/images/skills/css.png" alt="css"><figcaption>CSS</figcaption></figure>
-      <figure><img src="../assets/images/skills/JS.png" alt="JavaScript"><figcaption>JavaScript</figcaption></figure>
-      <figure><img src="../assets/images/skills/vue.png" alt="Vue.js"><figcaption>Vue.js</figcaption></figure>
-      <figure><img src="../assets/images/skills/git.png" alt="git"><figcaption>Git</figcaption></figure>
-      <figure><img src="../assets/images/skills/github.png" alt="GitHub"><figcaption>GitHub</figcaption></figure>
+        <figure><img src="../assets/images/skills/html.png" alt="html"><figcaption>HTML</figcaption></figure>
+        <figure><img src="../assets/images/skills/css.png" alt="css"><figcaption>CSS</figcaption></figure>
+        <figure><img src="../assets/images/skills/JS.png" alt="JavaScript"><figcaption>JavaScript</figcaption></figure>
+        <figure><img src="../assets/images/skills/vue.png" alt="Vue.js"><figcaption>Vue.js</figcaption></figure>
+        <figure><img src="../assets/images/skills/git.png" alt="git"><figcaption>Git</figcaption></figure>
+        <figure><img src="../assets/images/skills/github.png" alt="GitHub"><figcaption>GitHub</figcaption></figure>
+      </div>
     </div>
 
   </div>
@@ -41,10 +45,16 @@
 <style>
   .skills-wrapper{
     text-align:center;
+    .div{
+      padding-bottom: 20px;
+    }
   }
 
   .icon-area{
+    width: 64%;
+    margin: 0 auto;
     display: flex;
+    flex-wrap: wrap;
     justify-content: center;
     align-items: center;
   }
