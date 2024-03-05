69b84693064a217bf79d6a4971bc957b1efd776c
Naoya-Yasuda <2301330039zz@cyber-u.ac.jp>
2024-03-05 18:48:09 +0900

    アイコン修正その1

--- src/components/skill.vue
+++ src/components/skill.vue
@@ -4,10 +4,23 @@
 
     <div class="quolifications">
       <h3>- Quolifications -</h3>
-      <figure><img src="../assets/images/skills/MOS_Word.png" alt="MOS_Word"><figcaption>MOS Word</figcaption></figure>
-      <figure><img src="../assets/images/skills/MOS_Excel.png" alt="MOS_Excel"><figcaption>MOS Excel</figcaption></figure>
-      <figure><img src="../assets/images/skills/FE.gif" alt="Fundamental_Engineer"><figcaption>基本情報技術者</figcaption></figure>
-      <figure><img src="../assets/images/skills/Oracle_bronze.png" alt="Oracle_Bronze"><figcaption>Oracle Bronze</figcaption></figure>
+      <figure><img src="../assets/images/skills/CSM-logo.png" alt="Certified_ScrumMaster"><figcaption>スクラムマスター</figcaption></figure>
+      <figure><img src="../assets/images/skills/SAA-logo.png" alt="AWS_Certified_Solutions_Architect"><figcaption>AWSソリューションアーキテクト</figcaption></figure>
+      <figure><img src="../assets/images/skills/AP-logo.png" alt="Applied_Information_Technology_Engineer"><figcaption>応用情報技術者</figcaption></figure>
+      <figure><img src="../assets/images/skills/FE-logo.png" alt="Fundamental_Engineer"><figcaption>基本情報技術者</figcaption></figure>
+      <figure><img src="../assets/images/skills/LPIC-1-logo.png" alt="LPIC_1"><figcaption>LPIC-1</figcaption></figure>
+      <figure><img src="../assets/images/skills/OCJP-silver-logo.webp" alt="Oracle_Certified_Java_Programmer_Silver"><figcaption>OCJP-Silver</figcaption></figure>
+      <figure><img src="../assets/images/skills/tokei-kentei-4-logo.png" alt="Japan_Statistical_Society_Certificate_4"><figcaption>統計検定4級</figcaption></figure>
+    </div>
+
+    <div class="activity">
+      <h3>- Activity -</h3>
+      <figure><img src="../assets/images/skills/Hack_U_Online.png" alt="Hack_U_Online"><figcaption>Hack U Online2023 最優秀賞・HappyHacking賞</figcaption></figure>
+      <figure><img src="../assets/images/skills/MDXQ-1.png" alt="MDXQ_Term1"><figcaption>経産省主催マナビDX Quest2023 Term1 Gold修了</figcaption></figure>
+      <figure><img src="../assets/images/skills/MDXQ-2.png" alt="MDXQ_Term2"><figcaption>経産省主催マナビDX Quest2023 Term2 地域協業プログラム修了</figcaption></figure>
+      <figure><img src="../assets/images/skills/zero-to-one-logo.png" alt="zero_to_one"><figcaption>JDLA「E資格」向け認定プログラム 修了</figcaption></figure>
+      <figure><img src="../assets/images/skills/kaggle-logo.png" alt="kaggle"><figcaption>Kaggleコンペ 参加中</figcaption></figure>
+      <figure><img src="../assets/images/skills/SIGNATE-logo.png" alt="zero_to_one"><figcaption>SIGNATEコンペ 参加中</figcaption></figure>
     </div>
 
     <div class="languages">
@@ -19,7 +32,7 @@
       <figure><img src="../assets/images/skills/git.png" alt="git"><figcaption>Git</figcaption></figure>
       <figure><img src="../assets/images/skills/github.png" alt="GitHub"><figcaption>GitHub</figcaption></figure>
     </div>
-    
+
   </div>
 </template>
 
@@ -36,7 +49,7 @@
     display:inline-block;
     margin-bottom:30px;
   }
-  
+
   figcaption{
     color:grey;
     font:10px;
@@ -48,7 +61,7 @@
     height: 70px;
   }
 
-  @media (max-width: 767px) { 
+  @media (max-width: 767px) {
 
     figure{
       width: 112px;
